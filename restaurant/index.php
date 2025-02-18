d05c800ec100a187052ecad89dbf1fd48ef5646b
AlimiChourouk <Alimi.Chourouk.solicode@gmail.com>
2025-02-18 12:24:19 +0100

    Add files via upload

---
+++ restaurant/index.php
@@ -0,0 +1,33 @@
+<?php
+        $restaurants="";
+        require("fech.php");
+        $sql = "SELECT * FROM plat";
+        $stmt = $pdo->query($sql);
+        $plats = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($plats as $plat) {
+            $restaurants .= "<img src=\"{$plat['image']}\" alt=\"\"> "; 
+            $restaurants .= "<p>" . $plat['nomPlat'] . "</p>";  
+            $restaurants .= "<p>" . $plat['categoriePlat'] . "</p>";  
+            $restaurants .= "<p>" . $plat['TypeCuisine'] . "</p>";  
+            $restaurants .= "<p>" . $plat['prix'] . "</p>";  
+        }
+?>
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Restaurants</h1>
+    <div>
+        <?= $restaurants ;
+        ?>
+    </div>
+    <img src="" alt="">
+</body>
+</html>
