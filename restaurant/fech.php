1f0b1df9719f5dfe5a5919122e6ab3cb3d35be0c
AlimiChourouk <Alimi.Chourouk.solicode@gmail.com>
2025-02-19 15:13:24 +0000

    Update fech.php

--- restaurant/fech.php
+++ restaurant/fech.php
@@ -3,12 +3,10 @@ $host = "localhost";
 $dbname = "SoliRestaurant";
 $username = "root";
 $password = "";
-$port = 8889; // Utilisez 3306 si c'est le port par défaut
 
 try {
-    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connexion réussie !";
 } catch (PDOException $e) {
     die("Erreur : " . $e->getMessage());
 }
